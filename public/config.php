5562b3466435db4824e5945176922661f21a540d
oscardona777 <cardona.cardona@gmail.com>
2025-06-24 21:43:11 -0500

    Update config.php

--- public/config.php
+++ public/config.php
@@ -3,7 +3,7 @@ define('PAYMENTEZ_APP_CODE', 'TESTECUADORSTG-EC-CLIENT');
 define('PAYMENTEZ_APP_KEY', 'd4pUmVHgVpw2mJ66rWwtfWaO2bAWV6X');
 define('PAYMENTEZ_SERVER_APP_CODE', 'TESTECUADORSTG-EC-SERVER');
 define('PAYMENTEZ_SERVER_APP_KEY', '67vVmLALRrbSaQHiEer40gjb49peos');
-define('PAYMENTEZ_SANDBOX', true); // false en producción
+define('PAYMENTEZ_SANDBOX', true); // false en producció
 
 function get_headers_auth() {
     $timestamp = time();
@@ -18,3 +18,4 @@ function api_url($endpoint) {
     return (PAYMENTEZ_SANDBOX ? "https://ccapi-stg.paymentez.com" : "https://ccapi.paymentez.com") . $endpoint;
 }
 ?>
+
