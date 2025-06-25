4204d58dc41949bb05c64f02c604234b08507f40
oscardona777 <cardona.cardona@gmail.com>
2025-06-24 21:24:08 -0500

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,18 +1,20 @@
 <?php
 define('PAYMENTEZ_APP_CODE', 'TESTECUADORSTG-EC-CLIENT');
 define('PAYMENTEZ_APP_KEY', 'd4pUmVHgVpw2mJ66rWwtfWaO2bAWV6X');
-define('PAYMENTEZ_SANDBOX', true);
+define('PAYMENTEZ_SERVER_APP_CODE', 'YOUR_SERVER_APP_CODE');
+define('PAYMENTEZ_SERVER_APP_KEY', 'YOUR_SERVER_APP_KEY');
+define('PAYMENTEZ_SANDBOX', true); // false en producción
 
 function get_headers_auth() {
     $timestamp = time();
-    $token_string = PAYMENTEZ_APP_CODE . ";" . $timestamp . ";" . hash_hmac('sha256', $timestamp, PAYMENTEZ_APP_KEY);
-    $auth_token = base64_encode($token_string);
+    $auth_token = base64_encode(PAYMENTEZ_SERVER_APP_CODE . ";" . $timestamp . ";" . hash_hmac('sha256', $timestamp, PAYMENTEZ_SERVER_APP_KEY));
     return [
-        "Auth-Token: " . $auth_token,
-        "Content-Type: application/json"
+        'Auth-Token: ' . $auth_token,
+        'Content-Type: application/json'
     ];
 }
 
 function api_url($endpoint) {
     return (PAYMENTEZ_SANDBOX ? "https://ccapi-stg.paymentez.com" : "https://ccapi.paymentez.com") . $endpoint;
 }
+?>
