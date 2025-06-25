8c067653c0dd943786fa5f6bb7302474b0c0e2f6
oscardona777 <cardona.cardona@gmail.com>
2025-06-24 21:35:52 -0500

    Update config.php

--- public/config.php
+++ public/config.php
@@ -7,14 +7,14 @@ define('PAYMENTEZ_SANDBOX', true); // false en producción
 
 function get_headers_auth() {
     $timestamp = time();
-    $token_string = PAYMENTEZ_APP_CODE . ";" . $timestamp . ";" . hash_hmac('sha256', $timestamp, PAYMENTEZ_APP_KEY);
-    $auth_token = base64_encode($token_string);
+    $auth_token = base64_encode(PAYMENTEZ_SERVER_APP_CODE . ";" . $timestamp . ";" . hash_hmac('sha256', $timestamp, PAYMENTEZ_SERVER_APP_KEY));
     return [
-        "Auth-Token: " . $auth_token,
-        "Content-Type: application/json"
+        'Auth-Token: ' . $auth_token,
+        'Content-Type: application/json'
     ];
 }
 
 function api_url($endpoint) {
     return (PAYMENTEZ_SANDBOX ? "https://ccapi-stg.paymentez.com" : "https://ccapi.paymentez.com") . $endpoint;
 }
+?>
